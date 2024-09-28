7a768a66d5f7a282a2ae43b433395e42eb36ec70
stackgen-automation <automation@stackgen.com>
2024-09-28 09:47:20 +0000

    Automated commit

---
+++ terraform/terraform/README.md
@@ -0,0 +1,3 @@
+# README
+This is a readme file for IaC generated with appCD.
+You can modify your appStack -> [here](http://cloud.stackgen.com/appstacks/c1aad378-bd9f-4df1-afab-026579af3f8e)
---
+++ terraform/terraform/outputs.tf
@@ -0,0 +1,71 @@
+
+output "aws_cloudwatch_log_group_aws_lambda_hello_kitty_anwar_function_arn" {
+  value = module.appcd_6dcc6098-38ee-5ca3-8e62-28b2cb2ab376.arn
+  sensitive = false
+}
+
+
+output "aws_cloudwatch_log_group_aws_lambda_hello_kitty_anwar_function_name" {
+  value = module.appcd_6dcc6098-38ee-5ca3-8e62-28b2cb2ab376.name
+  sensitive = false
+}
+
+
+output "aws_s3_anwar_hellokitty_bucket_website_endpoint" {
+  value = module.appcd_50a4ecb8-6b13-5a66-926d-deceb6e930a2.bucket_website_endpoint
+  sensitive = false
+}
+
+
+output "aws_s3_anwar_hellokitty_kms_arn" {
+  value = module.appcd_50a4ecb8-6b13-5a66-926d-deceb6e930a2.kms_arn
+  sensitive = false
+}
+
+
+output "aws_s3_anwar_hellokitty_arn" {
+  value = module.appcd_50a4ecb8-6b13-5a66-926d-deceb6e930a2.arn
+  sensitive = false
+}
+
+
+output "aws_s3_anwar_hellokitty_bucket_name" {
+  value = module.appcd_50a4ecb8-6b13-5a66-926d-deceb6e930a2.bucket_name
+  sensitive = false
+}
+
+
+output "aws_lambda_hello_kitty_anwar_function_function_url" {
+  value = module.appcd_3c528139-bf51-5784-be6a-41739764a1c1.function_url
+  sensitive = false
+}
+
+
+output "aws_lambda_hello_kitty_anwar_function_invoke_arn" {
+  value = module.appcd_3c528139-bf51-5784-be6a-41739764a1c1.invoke_arn
+  sensitive = false
+}
+
+
+output "aws_lambda_hello_kitty_anwar_function_function_arn" {
+  value = module.appcd_3c528139-bf51-5784-be6a-41739764a1c1.function_arn
+  sensitive = false
+}
+
+
+output "aws_lambda_hello_kitty_anwar_function_function_name" {
+  value = module.appcd_3c528139-bf51-5784-be6a-41739764a1c1.function_name
+  sensitive = false
+}
+
+
+output "aws_iam_role_hello_kitty_anwar_role_arn" {
+  value = module.appcd_0d86a85f-1ec0-5b09-8f19-4bdd2c16fb2f.arn
+  sensitive = false
+}
+
+
+output "aws_iam_role_hello_kitty_anwar_role_name" {
+  value = module.appcd_0d86a85f-1ec0-5b09-8f19-4bdd2c16fb2f.name
+  sensitive = false
+}
